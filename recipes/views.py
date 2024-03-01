d1c9e2456cf803351b5ad80cad1db9341be0d016
Eduardo Cales Ferreira <dudu.calesferreira@gmail.com>
2024-02-29 22:42:20 -0300

    removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,6 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Eduardo Cales',
     })
-
-def contato(request):
-    return render(request, 'me-apague/temp.html')
-
-def sobre(request):
-    return HttpResponse('SOBRE')
